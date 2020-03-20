4b980448df534c88b3ba84ca79a60d958be134f6
rishabhkharbanda <techie448@gmail.com>
2020-03-19 22:35:22 -0400

    api

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  state = {
+    person: null,
+  };
+  async componentDidMount(){
+    const res = await fetch("https://assignment-nodejs.herokuapp.com/api/phone");
+    const data = await res.json();
+    console.log(data);
+    this.setState({person:data[0].model});
+  };
+
+  render() {
+
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <div>
+              {!this.state.person ? <p> Loading...</p> : <div> {this.state.person} </div>}
+            </div>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    )
+  }
 }
 
 export default App;
